af736e2b7fd894a047801b939a36e8529f2020a8
cvbn2000 <zcmesser@gmail.com>
2016-03-10 11:18:31 -0500

    Added files via upload

---
+++ src/main/java/com/github/necromaticdescent/NecromaticDescent.java
@@ -0,0 +1,27 @@
+package com.github.necromaticdescent;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.github.necromaticdescent.help.Reference;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class NecromaticDescent
+{@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	
+	}
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+{
+	
+}
+	@Mod.EventHandler
+	public void init(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
---
+++ src/main/java/com/github/necromaticdescent/help/Reference.java
@@ -0,0 +1,8 @@
+package com.github.necromaticdescent.help;
+
+public class Reference 
+{
+public static final String MODID= "necromaticdescent";
+public static final String NAME = "NecromaticDescent";
+public static final String VERSION = "a.0.0.1";
+}
